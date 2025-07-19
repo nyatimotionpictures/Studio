13d655638dafe40c32b4ef054735b845ec9d31cf
joshbaz <jkimbareeba@yahoo.com>
2025-07-19 20:25:51 +0300

    Updated the socket comments

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -2,10 +2,12 @@ import {io } from "socket.io-client";
 import { sockeUrl } from "../3-Middleware/apiRequest";
 
 // Configure the socket connection
+//localhost
 // const socket = io(`ws://${sockeUrl}`, {
 //     autoConnect: false, // Prevent auto-connection; connect explicitly when needed
 //   });
 
+  //production
 const socket = io(`${sockeUrl}`, {
   autoConnect: false, // Prevent auto-connection; connect explicitly when needed
 });
