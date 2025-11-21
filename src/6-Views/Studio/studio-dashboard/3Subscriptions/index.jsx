1b0eec0c76ee12bf73688d20bd4db1f9a0a16870
Kimbareeba <jkimbareeba@yahoo.com>
2025-11-21 15:56:59 +0300

    Added Tabs to the equation to show both Failed and those Pending/success

--- src/6-Views/Studio/studio-dashboard/3Subscriptions/index.jsx
+++ src/6-Views/Studio/studio-dashboard/3Subscriptions/index.jsx
@@ -11,14 +11,25 @@ import fullhdIcon from '../../../../1-Assets/icons/fullhd-resolution.svg'
 import ultrahdIcon from '../../../../1-Assets/icons/ultrahd-resolution.svg'
 import SubscriptionTable from "../../../../2-Components/Tables/SubscriptionTable.jsx";
 import { useGetPurchases } from "../../../../5-Store/TanstackStore/services/queries.ts";
+import { Tab, Tabs } from "@mui/material"
+import { TabContext, TabPanel } from '@mui/lab'
+import SubscriptionFailedListTable from "../../../../2-Components/Tables/SubscriptionFailedListTable.jsx";
 
-
+const displayTabs = [
+  {
+    title: "Pending/Success",
+    position: "1"
+  },
+  {
+    title: "Failed",
+    position: "2"
+  }
+]
 
 const Subscriptions = () => {
-  const [openFilmModal, setOpenFilmModal] = React.useState(false);
+  const [currentTabValue, setCurrentTabValue] = React.useState('1')
   let navigate = useNavigate();
 
-  const videoRef = useRef(null);
   const [statsArray, setStatsArray] = useState([
     {
       title: "Total Paid Subscriptions",
@@ -30,45 +41,56 @@ const Subscriptions = () => {
   let getallpurchases = useGetPurchases();
 
 
-  console.log(getallpurchases.data)
+  // console.log('purchases', getallpurchases.data)
 
   useEffect(() => {
-    if(getallpurchases.data) {
+    if (getallpurchases.data) {
       let successPurchases = [];
-     
-      const filterPurchases =   getallpurchases.data?.transactions?.filter((data) =>{
 
-        if (data.status.includes("success")){
+      const filterPurchases = getallpurchases.data?.transactions?.filter((data) => {
+
+        if (data.status.includes("SUCCESS")) {
           successPurchases.push(parseFloat(data.amount))
         }
-      }  ) ?? [];
+      }) ?? [];
 
 
-  
-      const ReducerPurchases = successPurchases?.length > 0 ?  successPurchases?.reduce((sum, price) => sum + price, 0  ) : 0;
-   
-   
-      setStatsArray(()=> [
+
+      const ReducerPurchases = successPurchases?.length > 0 ? successPurchases?.reduce((sum, price) => sum + price, 0) : 0;
+
+
+      setStatsArray(() => [
         {
           title: "Total Paid Subscriptions",
-          stats: `UGX ${ReducerPurchases}`,
-          
+          stats: `UGX ${getallpurchases.data?.totalAmount || ReducerPurchases}`,
+
           icon: false,
         }
-        
+
       ])
     }
   }, [getallpurchases.data])
 
-  
-  
+  //handle tab change
+  const handleTabChange = (event, newValue) => {
+    setCurrentTabValue(() => newValue);
+  }
 
-  const formRef = React.useRef();
+  const TabDisplay = (datakey) => {
+    switch (datakey) {
+      case "Pending/Success":
+        return <SubscriptionTable transactions={getallpurchases?.data?.transactions} />
+      case "Failed":
+        return <SubscriptionFailedListTable transactions={getallpurchases?.data?.failedtransactions} />
+      default:
+        break;
+    }
+  }
 
 
 
 
- 
+
   return (
     <div className="max-h-screen h-[100vh] w-full flex flex-col bg-whites-900 relative">
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 relative overflow-auto">
@@ -135,15 +157,48 @@ const Subscriptions = () => {
             })}
           </div>
 
-          {/** table */}
-          <div className="pt-7 pb-11 ">
-            {/* <VideoListTable /> */}
+          {/** Tabs */}
+          {
+            displayTabs?.length > 0 && (
+              <TabContext value={currentTabValue !== null && currentTabValue}>
+                <div className="flex flex-col justify-end space-x-5    p-0 box-border bg-transparent relative">
+                  <hr className=' absolute -bottom-0 h-[1px] border-[transparent]  border-b-[0.1px] border-b-[#ee5170]   w-full z-10' />
+                  <Tabs value={currentTabValue !== null && currentTabValue} onChange={handleTabChange} indicatorColor='' sx={{ margin: "0px", }} >
+                    {displayTabs.map((data) => {
+                      return (
+                        <Tab className={`  font-[Inter-Regular] ${currentTabValue !== null && currentTabValue === data.position ? "!scale-100 text-whites-50 !border-b-2 !border-b-transparent z-40" : "scale-75 text-primary-500 text-opacity-75"}  translate-y-2 text-sm mx-2`} key={data.position} value={data.position} label={data.title} sx={currentTabValue !== null && currentTabValue === data.position ? { border: "2px solid #ee5070 !important", backgroundColor: "#24222a", borderBottom: "0px !important", borderTopLeftRadius: "10px", borderTopRightRadius: "10px" } : { border: "1px solid #424148 !important", borderBottom: "0px", borderTopLeftRadius: "10px", borderTopRightRadius: "10px" }} />
+                      )
+                    })}
+                  </Tabs>
+                </div>
 
-            <SubscriptionTable transactions={getallpurchases?.data?.transactions} />
+                {/** table */}
+                <div className="pt-7 pb-11 ">
+                  {/* <VideoListTable /> */}
+
+                  {/* <SubscriptionTable transactions={getallpurchases?.data?.transactions} /> */}
+
+                  {
+                    displayTabs.map((data) => {
+                      return (
+                        <TabPanel key={data.position} value={data.position}>
+                          {TabDisplay(data.title)}
+                        </TabPanel>
+                      )
+                    })
+                  }
+                </div>
+              </TabContext>
+            )
+          }
+          <div>
+
           </div>
+
+
         </div>
       </div>
-    
+
 
     </div>
   )
